128b6be8bc6c6ab1e17b91e94320f4d649877b8f
Arnold <Arnold01@live.no>
2024-08-26 02:14:31 +0200

    Added google analytics to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/home/Navbar";
+import Script from "next/script"; // Import Script component
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,6 +12,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-ZPEYW7NDPH"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-ZPEYW7NDPH');
+          `}
+        </Script>
+      </head>
       <body className={inter.className}>
         <Navbar />
         {children}
