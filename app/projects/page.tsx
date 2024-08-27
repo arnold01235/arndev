c5e4d704994c85085a0dd6367662018fa55a2c91
Arnold <Arnold01@live.no>
2024-08-27 21:24:22 +0200

    Added more pictures in projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,4 +1,5 @@
 import ProjectCard from '@/components/projects/ProjectCard';
+import { Card } from '@/components/ui/card';
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -12,6 +13,8 @@ const projects = () => {
         <main className="gap-4 p-4 h-auto max-w-7xl mx-auto">
         <div>
         
+        <h1 className="text-4xl font-bold text-center">NB. Holder fortsatt på å ordne linkene og legge til flere prosjekter</h1>
+        
         {/*Portfolio Website*/}
         <ProjectCard 
         title='Portfolio Website' 
@@ -20,7 +23,7 @@ const projects = () => {
         githubUrl='https://github.com/your-repo-url'
         liveUrl='https://your-live-demo-url'
         youtubeUrl='https://your-video-demo-url'
-        imageUrl='/images/portfolio-website.png'
+        imageUrl='/images/Arndev.png'
         details={`Denne portfolio-nettsiden ble utviklet med Next.js og React for å vise frem mine prosjekter og erfaringer. Ved å bruke ShadCN og Tailwind CSS fikk jeg en ren og moderne design. Nettsiden er også responsiv, slik at den ser bra ut på alle enheter. Jeg brukte Vercel for å deploye nettsiden, noe som gjorde det enkelt å oppdatere og vedlikeholde.
 
             Hovedårsaken til at jeg opprettet denne portfoliosiden var for å enkelt holde oversikt over mine prosjekter, spesielt de som ikke er tilgjengelige på GitHub. I tillegg ønsket jeg å styrke mine ferdigheter innen front-end utvikling, siden jeg primært har jobbet med back-end utvikling og plattformutvikling i forskjellige prosjekter.`}
@@ -36,7 +39,7 @@ const projects = () => {
       githubUrl='https://github.com/your-repo-url'
       liveUrl='https://your-live-demo-url'
       youtubeUrl='https://your-video-demo-url'
-      imageUrl='/images/logic-gate-vr.png'
+      imageUrl='/images/Logicgate.png'
       details={`Her så jobbet jeg sammen med en student for å utvikle en simulator for å lage og teste logiske kretser i VR. Målet med dette prosjektet var for å gjøre læringen av logiske kretser mer interaktiv og visuell. Ved hjelp av verktøyene Unity tilbyr hadde jeg muligten til å lage en vr applikasjon relativt lett. For å skape modellene brukte vi Blender, et gratis 3D-modelleringsverktøy. Slik kunne vi lage enkle modeller av de logiske portene som AND, OR, NOT og XOR.
 
           Resultatet ble en applikasjon som lar brukeren lage logiske kretser ved å plassere ned portene og koble de sammen. Utfordringen med dette prosjektet var å lage en intuitivt opplevelse for brukeren. Jeg har lite erfaring med VR spill, så det ble litt utfordrende å lage en god brukeropplevelse når det kommer til måten man bevenger seg og interagerer med objektene i spillet.
